a6dd4e8bb07af049e788eb9aab2cb5ca23ce5052
Abdul Qadir <AQshahid43@gmail.com>
2021-08-16 19:30:02 +0500

    first commit

---
+++ app/app.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text
+} from 'react-native';
+
+
+
+const App: () => React$Node = props => {
+  
+  return (
+    <SafeAreaView>
+      <View
+          style={{
+            backgroundColor: 'white',
+          }}>
+          <Text>Boilerplate</Text>
+         
+        </View>
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// // import {Provider} from 'react-redux';
+// import App from './App';
+// // export default App;
+// // import store from '../src/redux/store';
+
+// const AppContainer: () => React$Node = () => {
+//   return (
+//       <App />
+  
+//   );
+// };
+
+// export default AppContainer;
